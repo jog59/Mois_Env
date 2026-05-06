56ead0ee34df087eb516176f4eaed6fe16dfa64e
jog59 <103459938+jog59@users.noreply.github.com>
2026-05-06 15:08:57 +0200

    Change audio file to 'fuite_air_comprimee.m4a'

--- js/detection.js
+++ js/detection.js
@@ -182,7 +182,7 @@ viewer.camera.add(listener);
 const sound = new THREE.PositionalAudio(listener);
 
 const audioLoader = new THREE.AudioLoader();
-audioLoader.load('assets/Voix 005.m4a', function (buffer) {
+audioLoader.load('fuite_air_comprimee.m4a', function (buffer) {
     sound.setBuffer(buffer);
     sound.setRefDistance(500);
     sound.setLoop(true);
