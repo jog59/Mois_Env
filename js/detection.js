605b3feaa307af0392e2149ca1a14805d489254a
jog59 <103459938+jog59@users.noreply.github.com>
2026-05-13 14:59:14 +0200

    Update console log message and adjust position

--- js/detection.js
+++ js/detection.js
@@ -1,4 +1,4 @@
-console.log("detection.js chargé !");
+console.log("detection.js chargé !!");
 
 /* LISTES DE HOTSPOTS */
 let hotspotsPano1 = [];
@@ -244,7 +244,7 @@ setTimeout(() => {
 
     const Zone2_4 = new THREE.Mesh(geo, mat);
     Zone2_4.name = "Air comprimée";
-    Zone2_4.position.set(-1875, 2460, 3000);
+    Zone2_4.position.set(-2000, 2100, 3000);
     Zone2_4.lookAt(new THREE.Vector3(0, 0, 0));
 
     Zone2_4.userData = {
