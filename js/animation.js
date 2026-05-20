6de1bab3878dd5b2442661d2fac5a9a68fc6a43d
jog59 <103459938+jog59@users.noreply.github.com>
2026-05-20 16:10:00 +0200

    Update sprite position and opacity in animation.js

--- js/animation.js
+++ js/animation.js
@@ -3,12 +3,15 @@ const texture = new THREE.TextureLoader().load("assets/fuite_air.gif");
 const material = new THREE.SpriteMaterial({
   map: texture,
   transparent: true
+    opacity: 1
 });
 
 const sprite = new THREE.Sprite(material);
 
+
 sprite.scale.set(500, 500, 1);
-sprite.position.set(1000, 500, 1000);
+sprite.position.set(0, 0, -2000);
+
 
 pano1.add(sprite);
-console.log("Animation fuite_air chargée");
+console.log("Animation fuite_air chargée!");
