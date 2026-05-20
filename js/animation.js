e258b3afc6c171c04629db59eeeb8f1dceaa3186
jog59 <103459938+jog59@users.noreply.github.com>
2026-05-20 16:29:48 +0200

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,18 +1,3 @@
-const texture = new THREE.TextureLoader().load("assets/fuite_air.gif");
-
-const material = new THREE.SpriteMaterial({
-  map: texture,
-  transparent: true
-});
-
-const sprite = new THREE.Sprite(material);
-
-
-sprite.scale.set(2500, 2500, 1);
-sprite.position.set(0, 0, -2000);
-
-
-pano1.add(sprite);
 console.log("Animation fuite_air chargée!!");
 
 
