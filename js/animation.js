9a061385bd0f1349f9aa38fcf0c2d3d379729926
jog59 <103459938+jog59@users.noreply.github.com>
2026-05-21 11:59:42 +0200

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,4 @@
-console.log("Animation fuite_air chargée");
+console.log("Animation fuite_air chargée!");
 
 
 const texture = new THREE.TextureLoader().load("assets/wind.png");
@@ -60,6 +60,6 @@ function animateReveal() {
 
 animateReveal();
 
-sprite.position.set(0, 0, -3000);
+sprite.position.set(500, 0, -3000);
 // sprite.scale.set(2000,2000,1);
 pano1.add(sprite);
