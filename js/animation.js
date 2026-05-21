dc7baec4d033a1f7ea9909757edbd99c123d969a
jog59 <103459938+jog59@users.noreply.github.com>
2026-05-21 14:44:50 +0200

    Adjust sprite position in animation

--- js/animation.js
+++ js/animation.js
@@ -62,7 +62,7 @@ function animateReveal() {
 
 animateReveal();
 
-sprite.position.set(4550, -1350, -1400);
+sprite.position.set(4600, -1350, -1400);
 sprite.lookAt(viewer.camera.position);
 sprite.scale.set(0.25, 0.25, 1);
 
