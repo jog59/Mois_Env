0f10415cc5445351e4540f21a5163ac63062e47c
jog59 <103459938+jog59@users.noreply.github.com>
2026-06-02 14:42:58 +0200

    Rename panelId for pano4 hotspots

--- js/detection.js
+++ js/detection.js
@@ -716,7 +716,7 @@ createHotspot(pano4, hotspotsPano4, {
 createHotspot(pano4, hotspotsPano4, {
     w: 1200, h: 3100,
     x: -3800, y: -2420, z: 1300,
-    panelId: "dechets",
+    panelId: "dechets_pano4",
     groupId: "dechets",
     rotationX: -5
 });
