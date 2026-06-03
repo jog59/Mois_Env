e7b9194ca5112b10ea3881621755e4465c3048c6
jog59 <103459938+jog59@users.noreply.github.com>
2026-06-03 10:17:02 +0200

    Update detection.js

--- js/detection.js
+++ js/detection.js
@@ -540,11 +540,20 @@ createHotspot(pano2, hotspotsPano2, {
 
 // éclairage2
 createHotspot(pano2, hotspotsPano2, {
-    w: 200, h: 10,
+    w: 200, h: 30,
     x: -144, y: 389, z: -278,
     panelId: "pano2_eclairage",
     groupId :"groupe_eclairage_pano2",
-    rotationX: 0, rotationY: 0, rotationZ: -20
+    rotationX: 0, rotationY: 0, rotationZ: -50
+});
+
+// éclairage3
+createHotspot(pano2, hotspotsPano2, {
+    w: 20, h: 90,
+    x: -384, y: 276, z: -162,
+    panelId: "pano2_eclairage",
+    groupId :"groupe_eclairage_pano2",
+    rotationX: 0, rotationY: 0, rotationZ: -50
 });
 
 /* PANORAMA 3 */
